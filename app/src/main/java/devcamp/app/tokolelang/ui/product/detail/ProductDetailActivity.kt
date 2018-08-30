c616172b243fef11888801b525edfbcf59ee1bcb
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-08-30 14:57:54 +0700

    appbarLayout handling for state of color

--- app/src/main/java/devcamp/app/tokolelang/ui/product/detail/ProductDetailActivity.kt
+++ app/src/main/java/devcamp/app/tokolelang/ui/product/detail/ProductDetailActivity.kt
@@ -1,7 +1,10 @@
 package devcamp.app.tokolelang.ui.product.detail
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import devcamp.app.tokolelang.R
 import devcamp.app.tokolelang.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_product_detail.*
 
 /**
  * Created by isfaaghyth on 8/30/18.
@@ -13,7 +16,18 @@ class ProductDetailActivity: BaseActivity<ProductDetailPresenter>(), ProductDeta
     override fun contentView(): Int = R.layout.activity_product_detail
 
     override fun onCreated() {
-
+        setNavigationUp(toolbar, true)
+        appBarLayout.addOnOffsetChangedListener { _, verticalOffset ->
+            if (verticalOffset <= -imgProduct.height / 2) {
+                toolbar.navigationIcon?.setColorFilter(Color.parseColor("#34495E"), PorterDuff.Mode.SRC_ATOP)
+                txtTitle.setTextColor(Color.parseColor("#34495E"))
+                txtTitle.text = "Product"
+            } else {
+                toolbar.navigationIcon?.setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_ATOP)
+                txtTitle.setTextColor(Color.parseColor("#FFFFFF"))
+                txtTitle.text = ""
+            }
+        }
     }
 
 }
